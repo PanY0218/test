d8d01aafd0e41b55e154d4c741896090b11282a1
PanY0218 <40119179+PanY0218@users.noreply.github.com>
2022-04-08 23:37:25 +0800

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,13 @@
+/**
+ * @author : pan.yuan2
+ * @version : 1.0
+ * @Description :
+ * @taskId : 000000
+ * @CreateDate : 22:38 2022/4/8
+ * @see : PACKAGE_NAME
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("test11");
+    }
+}
